7394e8fcda7bc6e3914f46a6fb4d3aedbc07a1bf
Kal <kalcrazyfj@gmail.com>
2023-01-21 00:31:33 +1200

    Added rating to thank you screen working

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function changeState() {
     const url = window.location.href;
     var ratingID= url.substring(url.lastIndexOf('#') + 1);
     let rating = document.getElementById(ratingID).innerText;
-    document.getElementById("rating").innerText = rating;
+    document.getElementById("rating").innerText = "You selected " + rating + " out of 5";
 
     const getOnClick = document.querySelector("#onclick");
     const initial = document.querySelector('#initial');
