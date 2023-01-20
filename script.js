ea886fdeb13f77041beed080378286d8205d0773
Kal <kalcrazyfj@gmail.com>
2023-01-20 18:58:14 +1200

    Added rating value in Thank you screen

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function changeState() {
+    const url = window.location.href;
+    var ratingID= url.substring(url.lastIndexOf('#') + 1);
+    let rating = document.getElementById(ratingID).innerText;
+    document.getElementById("rating").innerText = rating;
+
     const getOnClick = document.querySelector("#onclick");
     const initial = document.querySelector('#initial');
 
