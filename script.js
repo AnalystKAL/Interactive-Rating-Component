3a029e7bd1605235d0d626d288ae1030196f1643
Kal <kalcrazyfj@gmail.com>
2023-01-20 17:20:14 +1200

    Added JS function
    Hides initial state and shows thank you state on button click

---
+++ script.js
@@ -0,0 +1,10 @@
+function changeState() {
+    const getOnClick = document.querySelector("#onclick");
+    const initial = document.querySelector('#initial');
+
+    initial.style.display = "none";
+    getOnClick.style.display= "flex";
+}
+
+
+
